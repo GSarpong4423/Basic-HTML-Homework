848f4cc7019f48da7c9db7b0fd411ab391110321
Georgina Sarpong <georginasarpong.student@careerdevs.com>
2019-04-28 01:39:10 +0000

    variable pt.1

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert("Brownies are coming! Brownies are coming!");
+var name = "Georgina Sarpong";
+var waterType = " Poland Spring";
+var gender = "Female";
+var addressedAs = "she";
+var PricePerBottle = 3.80;
+var bottlesPerDay = 5;
+
+console.log("This is a console message");
+console.log(name, "likes", waterType, "and" ,addressedAs, "drinks", bottlesPerDay, "spending a total of", "$" + PricePerBottle * bottlesPerDay);
+
+// alert("Brownies are coming! Brownies are coming!");
